27616a4a2262ad1883bc958bc30ddf59c5ef6090
John Zonneveld <jjczon@gmail.com>
2020-03-28 22:14:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+    i = 2
+    if num <= 1
+        return false
+    end
+    while (i * i) <= num do
+        if num % i == 0
+          return false
+        end
+        i = i + 1
+        end
+    return true
+end
